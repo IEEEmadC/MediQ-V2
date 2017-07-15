a82798b109e956914f437c738ca3456e3da1e483
wasuradananjith <wasuradananjith@gmail.com>
2017-07-15 17:11:56 +0530

    first upload
    first

---
+++ MediQDB Server - hostbuddy.net/conn.php
@@ -0,0 +1,8 @@
+<?php
+$db_name="db_a27e51_mediq";
+$mysql_username="a27e51_mediq";
+$mysql_password="trycatch123";
+$server_name="mysql6001.hostbuddy.com";
+$conn = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
+
+?>
